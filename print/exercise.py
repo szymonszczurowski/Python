8ef8ba7cd28f74d0080244eefcf972a5091e0534
Szymon Szczurowski <szczurowski.szymon@wp.pl>
2023-07-05 16:20:37 +0200

    function print

---
+++ print/exercise.py
@@ -0,0 +1,10 @@
+print('TVN', 'POLSAT', 'BBC', 'HBO', 'MTV', sep=';')
+
+print('I like computers' ,'TVN','Polsat','BBC','HBO','MTV', sep=' but better is ')
+
+ProgramName = 'BBC'
+Item = 'News'
+Time = '18:00'
+
+print("I like watching News at:", ProgramName, Item, Time, ".")
+print('I like watching ',Item,' at ',Time,' on ',ProgramName, '.', sep='')
---
+++ print/main.py
@@ -0,0 +1,20 @@
+print('FR', 'USA', 'ESP', 'BE')
+france = 'FR'
+print(france, 'FR', 'USA', 'ESP', 'BE')
+
+pi = 3.14
+r = 10
+print(pi, r)
+
+print(pi, r, pi*r*r)
+print(pi, r, pi*r**2)
+
+print('Area of cycle with radius', r, '=', pi*r**2)
+
+print(1, 2, 3, sep='-')
+print(france, 'FR', 'USA', 'ESP', 'BE', sep="\n")
+print(france, 'FR', 'USA', 'ESP', 'BE', sep="\t")
+
+print("this is bell: \a")
+print("\u03A3")
+print("this is backslash: \\")
