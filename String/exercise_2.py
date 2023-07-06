c04c96c53c4c407a1d03e6f6a56074d225cf4fee
Szymon Szczurowski <szczurowski.szymon@wp.pl>
2023-07-06 11:25:08 +0200

    string_2

---
+++ String/exercise_2.py
@@ -0,0 +1,16 @@
+firstName = 'Kasia'
+familyName = 'Sowa'
+lastName = 'Mrugala'
+
+newName = firstName + ' ' + familyName + ' ' + lastName
+print(newName)
+
+music = 'Universal Fanfare" Jerry Goldsmith "Happy Together" Garry Bonner "I\'m a Man" Steve Winwood'
+print(music)
+
+music = 'Universal Fanfare" Jerry Goldsmith \n"Happy Together" Garry Bonner \n"I\'m a Man" Steve Winwood'
+print(music)
+
+print('(\\(\\')
+print('(-.-')
+print('O_(")(")')
---
+++ String/string_2.py
@@ -0,0 +1,15 @@
+drive = 'c:\\'
+folder = 'scripts\\python\\'
+file = 'myscript.py'
+path = drive  + folder + file
+print(path)
+
+justText = 'text with\nnewline'
+print(justText)
+
+justText = r'text with\nnewline' #surowy text
+
+justText = "Mc Donald's"
+print(justText)
+justText = 'Mc Donald\'s'
+print(justText)
