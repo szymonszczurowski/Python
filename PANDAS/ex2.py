d76621dacd618c1575bef566294d6c209e092eae
Szymon Szczurowski <szczurowski.szymon@wp.pl>
2023-08-23 00:52:15 +0200

    data frame

--- PANDAS/ex2.py
+++ PANDAS/ex2.py
@@ -1,24 +1,19 @@
 import pandas as pd
 import random as rd
+import numpy as np
 
-aircraftsDict = {
-    'PYT001':'Airbus 320',
-    'PYT002':'Boeing 737',
-    'PYT003':'Airbus 321'
-}
+fb = pd.read_csv("C:\\Users\\szczu\\Desktop\\data\\course-files\\mrbean_facebook_statuses_with_nulls.csv")
+print(fb.head())
 
-aircrafts = pd.Series(aircraftsDict)
-print(aircrafts.head())
+print(fb.info(memory_usage='deep'))
 
-flightsList = []
+fb = pd.read_csv("C:\\Users\\szczu\\Desktop\\data\\course-files\\mrbean_facebook_statuses_with_nulls.csv", usecols=[ "status_message", "status_type", "link_name",
+"num_reactions", "num_shares", "num_likes"])
 
-for i in range(100):
-    flightsList.append(rd.choice(aircrafts.index))
+print(fb.info(memory_usage='deep'))
+print(len(fb))
+print(fb["status_type"].value_counts())
 
-print(flightsList[:5])
+fb['status_type'] = fb["status_type"].astype('category')
 
-flights = pd.Series(flightsList)
-flights.head()
-flights_aircrafts = flights.map(aircrafts)
-
-print(flights_aircrafts)
+print(fb.info(memory_usage='deep'))
